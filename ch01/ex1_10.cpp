57b3c8206001728f0049963dab12d048f0d3dc7d
burlinchen(陈宝林) <burlinchen@tencent.com>
2016-10-16 12:03:16 +0800

    ex1 answer

--- ch01/ex1_10.cpp
+++ ch01/ex1_10.cpp
@@ -1,4 +1,13 @@
-int main()
-{
+// prints the numbers from ten down to zero.(use while)
+#include <iostream>
+
+using std::cout;
+
+int main() {
+    int i = 10;
+    while (i >= 0) {
+        cout << i-- << " ";
+    }
     return 0;
 }
+
--- ch01/ex1_11.cpp
+++ ch01/ex1_11.cpp
@@ -1,8 +1,23 @@
-//
-// Created by burlinchen on 16/10/16.
-//
+// Print each number in the range specified by two integers.
+#include <iostream>
 
-int main()
-{
+using std::cout;
+using std::cin;
+using std::endl;
+
+void print_range(int lo, int hi) {
+    if (lo > hi) {
+        return print_range(hi, lo);
+    }
+    for (int i = lo; i <= hi; i++) {
+        cout << i << " ";
+    }
+}
+
+int main() {
+    cout << "Enter two numbers:\n" << endl;
+    int v1 = 0, v2 = 0;
+    cin >> v1 >> v2;
+    print_range(v1, v2);
     return 0;
 }
