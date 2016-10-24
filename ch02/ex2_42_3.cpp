7b0beb3cbdb89944e8fcb8975837e97e7b9cd269
burlinchen(陈宝林) <burlinchen@tencent.com>
2016-10-24 08:12:12 +0800

    ch02 42

--- ch02/ex2_42_3.cpp
+++ ch02/ex2_42_3.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
+#include "./ex2_42.h"
 
 using std::cin;
 using std::cout;
 using std::endl;
+using namespace std;
 
-int main()
-{
+int main() {
+    Sales_data total;
+    double price1;
+    if (cin >> total.bookNo >> total.units_sold >> price1) {
+        total.CalcRevenue(price1);
+        Sales_data trans;
+        while (cin >> trans.bookNo >> total.units_sold >> price1) {
+            trans.CalcRevenue(price1);
+            if (total.bookNo == trans.bookNo) {
+                total.revenue += trans.revenue;
+            } else {
+                total.Print();
+                total = trans;
+            }
+        }
+        total.Print();
+    } else {
+        cerr << "no data" << endl;
+        return -1;
+    }
     return 0;
 }
